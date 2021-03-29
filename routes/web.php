f7bd49307d2707a227e618df8087fa9aeefdd192
irvanyanuar <irvanyanuar17@gmail.com>
2021-03-29 10:56:53 +0700

    Add template AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Facade\FlareClient\View;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/tes', function () {
+    return View('template.index');
 });
